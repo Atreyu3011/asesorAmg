c7001dd4990fb426c967952544574a92bcc1ab0b
Atreyu3011 <alejandro.murguia3011@gmail.com>
2025-10-13 19:28:45 -0600

    Initial commit

---
+++ src/app/api/layoutdata/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+const headerData = [
+  { label: "Inicio", href: "/" },
+  {
+    label: "Desarrollos",
+    href: "/properties/properties-list",
+    // submenu: [
+    //   { label: "Conoce nuestros desarrollos", href: "/properties/properties-list" },
+    //   // { label: "Property Details", href: "/properties/properties-list/modern-apartment" },
+    // ],
+  },
+  // {
+  //   label: "Blogs",
+  //   href: "#",
+  //   submenu: [
+  //     { label: "Blog Grid", href: "/blogs" },
+  //     { label: "Blog Details", href: "/blogs/blog_1" },
+  //   ],
+  // },
+  { label: "Contacto", href: "/contact" },
+  // { label: "Documentation", href: "/documentation" },
+];
+
+export const GET = async () => {
+  return NextResponse.json({
+    headerData
+  });
+};
+
---
+++ src/app/api/pagedata/route.ts
@@ -0,0 +1,112 @@
+import { NextResponse } from "next/server";
+
+const menuItems = [
+    { name: "Home", href: "#home" },
+    { name: "About", href: "#about" },
+    { name: "Services", href: "#services" },
+    { name: "Portfolio", href: "#portfolio" },
+    { name: "Testimonials", href: "#testimonials" },
+    { name: "Blog", href: "/#blog" },
+];
+
+const features = [
+    {
+        id: 1,
+        imgSrc: "/images/features/rating.svg",
+        title: "Great Experience",
+        description: "Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing Variou"
+    },
+    {
+        id: 2,
+        imgSrc: "/images/features/Give-Women's-Rights.svg",
+        title: "Great Experience",
+        description: "Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing Variou"
+    },
+    {
+        id: 3,
+        imgSrc: "/images/features/live-chat.svg",
+        title: "Great Experience",
+        description: "Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing Variou"
+    }
+];
+
+const searchOptions = {
+    keywords: [
+        { value: '', label: 'Palabras clave', placeholder: 'Palabras clave' },
+        // Add more keyword options as needed
+    ],
+    locations: [
+        { value: '', label: 'Ubicación' }, // Placeholder option
+        { value: 'Huatabampo, Sonora', label: 'Huatabampo, Sonora' },
+        { value: 'San Blas, El Fuerte, Sinaloa', label: 'San Blas, El Fuerte, Sinaloa' },
+        { value: 'Mazatlán, Sinaloa', label: 'Mazatlán, Sinaloa' },
+        { value: 'Santa María Huatulco, Oaxaca', label: 'Santa María Huatulco, Oaxaca' },
+        { value: 'Celestino Gasca, La Cruz de Elota, Sinaloa', label: 'Celestino Gasca, La Cruz de Elota, Sinaloa' },
+    ],
+    category : [
+        { value:'', label: 'Nombre del desarrollo' },
+        { value:'Nueva Camahuiroa', label: 'Nueva Camahuiroa' },
+        { value:'Nuevo Paraíso', label: 'Nuevo Paraíso' },
+        { value:'Aldea Naay', label: 'Aldea Naay' },
+        { value:'Cartagena de Indias', label: 'Cartagena de Indias' },
+        { value:'Nuevo Huatulco', label: 'Nuevo Huatulco' },
+        { value:'Hacienda Mónaco', label: 'Hacienda Mónaco' },
+        { value:'Hacienda Nápoles 2', label: 'Hacienda Nápoles 2' },
+        { value:'Nuevo Celestino', label: 'Nuevo Celestino' },
+    ],
+    // Define other options similarly
+};
+
+const data = [
+    {
+        src: "https://svgshare.com/i/187L.svg",
+        src1: "https://svgshare.com/i/183P.svg",
+        alt: "Image 1",
+        name: "Apartment",
+        count: 35,
+    },
+    {
+        src: "https://svgshare.com/i/188i.svg",
+        src1: "https://svgshare.com/i/185B.svg",
+        alt: "Image 2",
+        name: "Villa",
+        count: 15,
+    },
+    {
+        src: "https://svgshare.com/i/186r.svg",
+        src1: "https://svgshare.com/i/185n.svg",
+        alt: "Image 3",
+        name: "Office",
+        count: 26,
+    },
+    {
+        src: "https://svgshare.com/i/187Z.svg",
+        src1: "https://svgshare.com/i/184b.svg",
+        alt: "Image 4",
+        name: "Shop",
+        count: 43,
+    },
+    {
+        src: "https://svgshare.com/i/1881.svg",
+        src1: "https://svgshare.com/i/183k.svg",
+        alt: "Image 5",
+        name: "House",
+        count: 95,
+    },
+    {
+        src: "https://svgshare.com/i/188C.svg",
+        src1: "https://svgshare.com/i/184d.svg",
+        alt: "Image 6",
+        name: "Warehouse",
+        count: 18,
+    },
+];
+
+export const GET = async () => {
+  return NextResponse.json({
+    menuItems,
+    features,
+    searchOptions,
+    data
+  });
+};
